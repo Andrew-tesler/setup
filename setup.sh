93909301e9f03a4016141f820ba793b09d5e2a9e
Andrew-tesler <tesler.andrew@gmail.com>
2013-07-10 16:00:20 +0300

    Added setup.sh file

---
+++ setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Simple setup.sh for configuring Ubuntu 13.04
+# for headless setup. for my personal Linux computer
+
+
+
+# Install git
+sudo apt-get install -y git-core
+
+# Install apache2 server
+sudo apt-get install -y apache2
+
+# Install guake terinal
+sudo apt-get install -y guake
+
+# Install geany
+
+sudo apt-get install -y geany
+
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+npm install -g jshint
+
+# Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+sudo apt-get install -y rlwrap
+
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+sudo apt-add-repository -y ppa:cassou/emacs
+sudo apt-get update
+sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+
+
+
+cd $HOME
+# Make dotfiles dir in setup directory
+
+sudo mkdir /setup/dotfiles
+
+
+
+#if [ -d ./dotfiles/ ]; then
+#    mv dotfiles dotfiles.old
+#fi
+#if [ -d .emacs.d/ ]; then
+#    mv .emacs.d .emacs.d~
+#fi
+
+
+#ln -sb setup/dotfiles/.screenrc .
+#ln -sb setup/dotfiles/.bash_profile .
+#ln -sb setup/dotfiles/.bashrc .
+#ln -sb setup/dotfiles/.bashrc_custom .
+#ln -sf setup/dotfiles/.emacs.d .
+
